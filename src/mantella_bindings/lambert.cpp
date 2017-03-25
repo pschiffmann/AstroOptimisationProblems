ab69fc61dd11d8d821f352eb2a621ca7bc8da83b
Philipp Schiffmann <philippschiffmann93@gmail.com>
2017-03-25 15:01:45 +0100

    Move variable declarations to place of use in mga::lambert

--- src/mantella_bindings/lambert.cpp
+++ src/mantella_bindings/lambert.cpp
@@ -10,25 +10,14 @@ void multiple_gravity_assist::lambert(std::array<double, 3> r1,
                                       // OUTPUT
                                       std::array<double, 3>& v1,
                                       std::array<double, 3>& v2) {
-  double c,    // non-dimensional chord
-      s,       // non dimesnional semi-perimeter
-      am,      // minimum energy ellipse semi major axis
-      lambda,  // lambda parameter defined in Battin's Book
-      x, alfa, beta, psi, eta, eta2, sigma1, vr1, vt1, vt2, vr2;
-  int i;
-  const double tolerance = 1e-11;
-  double r2_vers[3];
-  double ih_dum[3], ih[3], dum[3];
-
-  // Increasing the tolerance does not bring any advantage as the
-  // precision is usually greater anyway (due to the rectification of the tof
-  // graph) except near particular cases such as parabolas in which cases a
-  // lower precision allow for usual convergence.
-
   if (t <= 0) {
     throw std::invalid_argument(
         "ERROR in Lambert Solver: Negative Time in input.");
   }
+
+  int i;
+  double r2_vers[3];
+  double ih_dum[3], ih[3], dum[3];
 
   double R = sqrt(dot_product(r1, r1));
   double V = sqrt(mu / R);
@@ -47,11 +36,19 @@ void multiple_gravity_assist::lambert(std::array<double, 3> r1,
 
   if (lw) theta = 2 * acos(-1.0) - theta;
 
-  c = sqrt(1 + r2_mod * (r2_mod - 2.0 * cos(theta)));
-  s = (1 + r2_mod + c) / 2.0;
-  am = s / 2.0;
-  lambda = sqrt(r2_mod) * cos(theta / 2.0) / s;
+  // non-dimensional chord
+  double c = sqrt(1 + r2_mod * (r2_mod - 2.0 * cos(theta)));
 
+  // non dimesnional semi-perimeter
+  double s = (1 + r2_mod + c) / 2.0;
+
+  // minimum energy ellipse semi major axis
+  double am = s / 2.0;
+
+  // lambda parameter defined in Battin's Book
+  double lambda = sqrt(r2_mod) * cos(theta / 2.0) / s;
+
+  double x;
   {
     // We start finding the log(x+1) value of the solution conic:
     // NO MULTI REV --> (1 SOL)
@@ -62,17 +59,23 @@ void multiple_gravity_assist::lambert(std::array<double, 3> r1,
     double y1 = log(x2tof(-.5233, s, c, lw)) - log(t);
     double y2 = log(x2tof(.5233, s, c, lw)) - log(t);
 
+    // Increasing the tolerance does not bring any advantage as the
+    // precision is usually greater anyway (due to the rectification of the tof
+    // graph) except near particular cases such as parabolas in which cases a
+    // lower precision allow for usual convergence.
+    const double tolerance = 1e-11;
+    double error = 1;
+
     // Newton iterations
-    double x_new = 0;
-    double err = 1;
-    while ((err > tolerance) && (y1 != y2)) {
+    double x_new, y_new;
+    while ((error > tolerance) && (y1 != y2)) {
       x_new = (x1 * y2 - y1 * x2) / (y2 - y1);
-      double y_new = logf(x2tof(expf(x_new) - 1, s, c, lw)) - logf(t);
+      y_new = logf(x2tof(expf(x_new) - 1, s, c, lw)) - logf(t);
       x1 = x2;
       y1 = y2;
       x2 = x_new;
       y2 = y_new;
-      err = fabs(x1 - x_new);
+      error = fabs(x1 - x_new);
     }
     x = expf(x_new) - 1;
   }
@@ -86,28 +89,30 @@ void multiple_gravity_assist::lambert(std::array<double, 3> r1,
 
   double a = am / (1 - x * x);
 
+  double eta, eta_squared;
   // psi evaluation
-  if (x < 1)  // ellipse
-  {
-    beta = 2 * asin(sqrt((s - c) / (2 * a)));
+  if (x < 1) {
+    // ellipse
+    double beta = 2 * asin(sqrt((s - c) / (2 * a)));
     if (lw) beta = -beta;
-    alfa = 2 * acos(x);
-    psi = (alfa - beta) / 2;
-    eta2 = 2 * a * pow(sin(psi), 2) / s;
-    eta = sqrt(eta2);
-  } else  // hyperbola
-  {
-    beta = 2 * asinh(sqrt((c - s) / (2 * a)));
+    double alfa = 2 * acos(x);
+    double psi = (alfa - beta) / 2;
+    eta_squared = 2 * a * pow(sin(psi), 2) / s;
+    eta = sqrt(eta_squared);
+  } else {
+    // hyperbola
+    double beta = 2 * asinh(sqrt((c - s) / (2 * a)));
     if (lw) beta = -beta;
-    alfa = 2 * acosh(x);
-    psi = (alfa - beta) / 2;
-    eta2 = -2 * a * pow(sinh(psi), 2) / s;
-    eta = sqrt(eta2);
+    double alfa = 2 * acosh(x);
+    double psi = (alfa - beta) / 2;
+    eta_squared = -2 * a * pow(sinh(psi), 2) / s;
+    eta = sqrt(eta_squared);
   }
 
   // parameter of the solution
-  double p = (r2_mod / (am * eta2)) * pow(sin(theta / 2), 2);
-  sigma1 = (1 / (eta * sqrt(am))) * (2 * lambda * am - (lambda + x * eta));
+  double vr1, vt1, vt2, vr2;
+  double p = (r2_mod / (am * eta_squared)) * pow(sin(theta / 2), 2);
+  vr1 = (1 / (eta * sqrt(am))) * (2 * lambda * am - (lambda + x * eta));
   vett(r1.data(), r2.data(), ih_dum);
   vers(ih_dum, ih);
 
@@ -115,7 +120,6 @@ void multiple_gravity_assist::lambert(std::array<double, 3> r1,
     for (i = 0; i < 3; i++) ih[i] = -ih[i];
   }
 
-  vr1 = sigma1;
   vt1 = sqrt(p);
   vett(ih, r1.data(), dum);
 
